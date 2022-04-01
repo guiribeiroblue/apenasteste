c69dd4e6c891e647eb06fbb4d0d41ee221a6dc65
Wheslley Rimar <wheslley_rimar@hotmail.com>
2022-04-01 09:15:31 -0300

    Subdividindo Componentes

--- src/components/PaletaLista/PaletaLista.jsx
+++ src/components/PaletaLista/PaletaLista.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import PaletaListaItem from 'components/PaletaListaItem/PaletaListaItem';
 import { paletas } from "mocks/paletas.js";
 import './PaletaLista.css';
 
@@ -16,29 +17,10 @@ function PaletaLista() {
         setPaletaSelecionada({...paletaSelecionada, ...paleta});
     }
 
-    const badgeCounter = (canRender, index) =>
-	    Boolean(canRender) && (<span className="PaletaListaItem__badge"> {paletaSelecionada[index]} </span>);
-
-
-    const removeButton = (canRender, index) =>
-	    Boolean(canRender) && (<button className="Acoes__remover" onClick={()=> removerItem(index)}>remover</button>);
-    
-    
+  
     return <div className="PaletaLista">
         {paletas.map((paleta, index) => (
-                <div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>
-                    {badgeCounter(paletaSelecionada[index], index)}
-                    <div>
-                        <div className="PaletaListaItem__titulo">{paleta.titulo}</div>
-                        <div className="PaletaListaItem__preco">{paleta.preco.toFixed(2)}</div>
-                        <div className="PaletaListaItem__descricao">{paleta.descricao}</div>
-                        <div className="PaletaListaItem__acoes Acoes">
-                            <button className={`Acoes__adicionar ${!paletaSelecionada[index] && "Acoes__adicionar--preencher"}`} onClick={() => adicionarItem(index)}>adicionar</button>
-                            {removeButton(paletaSelecionada[index], index)}
-                        </div>
-                    </div>
-                    <img className="PaletaListaItem__foto" src={paleta.foto} alt={`Paleta de ${paleta.sabor}`} />
-                </div>
+                <PaletaListaItem key={`PaletaListaItem-${index}`}/>
                 ))}
            </div>;
   }
