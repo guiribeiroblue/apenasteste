ede5fdf1644d1d6830a99a2a40fd7401ab300382
Wheslley Rimar <wheslley_rimar@hotmail.com>
2022-04-16 22:37:31 -0300

    Adicionando as rotas com react-router-dom.

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Home from './views/Home/Home';
+import App from "App";
 import 'assets/styles/main.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
